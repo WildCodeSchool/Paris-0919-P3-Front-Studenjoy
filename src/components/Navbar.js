a2e1ec1be08ffe8d6a8bdbfe99d8258aa80c6dad
Mboulgour <mathieu.r.77@gmail.com>
2019-12-16 13:23:34 +0100

    add navbar mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,21 +29,40 @@ class Navbar extends React.Component{
   render() {
     return (
       <>
-        <nav className="Navbar__nav">
-          <div className="Navbar__logo"><img src={this.state.mobile ? Logo : LogoText} alt="main-logo" /></div>
-          <ul className="Navbar__items">
-            {/* User not connected */}
-            <li className="Navbar__button">Connection</li>
-            <li className="Navbar__button Navbar__button--primary">Inscription</li>
-            {/* End user not connected */}
+        <nav className={this.state.mobile ? "Navbar__nav Navbar__nav--mobile" : "Navbar__nav"}>
+          <div className="Navbar__logo"><img src={LogoText} alt="main-logo" /></div>
+          {!this.state.mobile &&
+            <ul className="Navbar__items">
+              {/* User not connected */}
+              <li className="Navbar__button">Connection</li>
+              <li className="Navbar__button Navbar__button--primary">Inscription</li>
+              {/* End user not connected */}
 
-            {/* User connected */}
-            {/* <li className="Navbar__item">Item</li>
-            <li className="Navbar__item">Item</li>
-            <li className="Navbar__item">Item</li> */}
-            {/* End User connected */}
-          </ul>
+              {/* User connected */}
+              {/* <li className="Navbar__item">Item</li>
+              <li className="Navbar__item">Item</li>
+              <li className="Navbar__item">Item</li> */}
+              {/* End User connected */}
+            </ul>
+          }
         </nav>
+        {this.state.mobile &&
+          <div className="Navbar__mobile">
+            <ul className="Navbar__items_mobile">
+              {/* User not connected */}
+              <li className="Navbar__item_mobile Navbar__item_mobile--main">Connection</li>
+              <li className="Navbar__item_mobile Navbar__item_mobile--main">Inscription</li>
+              {/*  End User not connected */}
+
+              {/*  User connected */}
+              {/* <li className="Navbar__item_mobile">o</li>
+              <li className="Navbar__item_mobile">o</li>
+              <li className="Navbar__item_mobile">o</li>
+              <li className="Navbar__item_mobile">o</li> */}
+              {/*  End User connected */}
+            </ul>
+          </div>
+        }
       </>
     );
   }
