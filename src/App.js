ab7ca83172cc56982e7e3d547b68d817c6d00efd
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-12-17 17:14:38 +0100

    reorganized styles components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-
+import './styles/styles.scss';
 import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <>
       <Navbar />
-      
     </>
   );
-}
+};
 
 export default App;
