8bd493eca49fa9039bbd5a6b321d5a98057c07ad
Renemzadi45 <rene.mzadi45@gmail.com>
2020-01-06 14:22:40 +0100

    Revert "add cities cards to homepage -> carousel not done"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './styles/styles.scss';
 import Navbar from './components/Navbar';
-import CitiesCarousel from './components/CitiesCarousel';
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <CitiesCarousel />
     </>
   );
 };
