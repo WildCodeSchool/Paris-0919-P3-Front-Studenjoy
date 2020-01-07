193c777dc203021f44c81f5fb32947c74fecb571
Mboulgour <mathieu.r.77@gmail.com>
2020-01-07 14:59:26 +0100

    profil page + infos added -> need form to update profile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles/styles.scss';
-import Navbar from './components/Navbar';
 import CitiesCarousel from './components/CitiesCarousel';
 import SearchBar from './components/SearchBar';
 import HomeCarousel from './components/HomeCarousel';
@@ -9,7 +8,6 @@ import HomeCarousel from './components/HomeCarousel';
 const App = () => {
   return (
     <>
-      <Navbar />
       <HomeCarousel />
       <SearchBar />
       <CitiesCarousel />
