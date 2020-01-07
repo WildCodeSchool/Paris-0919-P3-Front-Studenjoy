bdb8db15c4e1d36b911cb4e7aa96ecf599cf6523
Mboulgour <mathieu.r.77@gmail.com>
2020-01-07 09:30:50 +0100

    add searchbar + mobile design + design

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
+class SearchBar extends React.Component {
+  render(){
+    return(
+      <div className="SearchForm__container">
+        <h1 className="SearchForm__title">Quel programme recherchez-vous ?</h1>
+        <div className="SearchForm__form">
+          <input
+              className="SearchForm__input"
+              placeholder="Specialités" 
+          />
+          <input
+              className="SearchForm__input"
+              placeholder="Ecoles, programmes" 
+          />
+          <input
+              className="SearchForm__input"
+              placeholder="Villes" 
+          />
+          <div className="SearchForm__submit_btn"><FontAwesomeIcon icon={faSearch} /></div>
+        </div>
+      </div>
+    )
+  };
+};
 
-
-
-export default class SearchBar extends React.Component {
-
-
-    render(){
-        return(
-
-            <div className="Container">
-            <h1>Quel programmes choisir</h1>
-            <div className="SearchForm">
-              <input
-                 className="Speciality__Search"
-                 placeholder="Speciality" 
-              />
-              <input
-                 className="School__Search"
-                 placeholder="Ecoles, programmes" 
-              />
-              <input
-                 className="Town__Search"
-                 placeholder="Villes" 
-              />
-            <div className="submitBtn"><i className="fa fa-search"></i></div>
-            </div>
-            </div>
-        )
-    }
-}
+export default SearchBar;
