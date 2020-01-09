8cce4dbbe611c08f61cabff553f269153b8c82a1
Mboulgour <mathieu.r.77@gmail.com>
2020-01-09 12:42:21 +0100

    ok commit

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -10,7 +10,7 @@ const UserProfile = () => {
       <div className="UserProfile__header">
         <div className="UserProfile__picture"><img src={Logo} alt="profile"/></div>
         <div className="UserProfile__header--main_infos">
-          <h1 className="UserProfile__title">$User Name</h1>
+          <h1 className="UserProfile__title">John Doe</h1>
           {/* <span>Profil complet <FontAwesomeIcon icon={faCheckCircle} /> </span> */}
           <span>Profil incomplet <FontAwesomeIcon icon={faTimesCircle} /></span>
         </div>
@@ -19,26 +19,26 @@ const UserProfile = () => {
         <h2 className="UserProfile__subtitle">Informations principales</h2>
         <div className="UserProfile__info">
           <span>Prénom</span>
-          <input type="text" disabled value="$User name"/>
+          <input type="text" disabled value="John"/>
         </div>
         <div className="UserProfile__info">
           <span>Nom</span>
-          <input type="text" disabled value="$User last name"/>
+          <input type="text" disabled value="Doe"/>
         </div>
         <div className="UserProfile__info">
           <span>E-mail</span>
-          <input type="text" disabled value="$User mail"/>
+          <input type="text" disabled value="john.doe@gmail.com"/>
         </div>
         <div className="UserProfile__info">
           <span>Date de naissance</span>
-          <input type="text" disabled value="$User birthdate"/>
+          <input type="text" disabled value="30/10/1995"/>
         </div>
 
         <h2 className="UserProfile__subtitle">Documents administratifs</h2>
         {/*  LOOP THROUGH USER DOC_ADMINS + SORT BY TYPE*/}
         <div className="UserProfile__doc">
-          <span>$Document type</span>
-          $Document name
+          <span>CV</span>
+          CVJohnDoe.pdf
         </div>
       </div>
     </div>
