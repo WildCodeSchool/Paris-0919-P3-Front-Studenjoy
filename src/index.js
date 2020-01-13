ef3f922ebdd869339c8d31cd29d36ebc6253e32a
Mboulgour <mathieu.r.77@gmail.com>
2020-01-13 11:29:08 +0100

    school list fix on cities

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import Sign from './components/Sign';
 import UserProfile from './components/UserProfile';
-import SchoolsList from './components/SchoolsList';
+import SchoolCards from './components/SchoolCards';
 
 
 ReactDOM.render(
@@ -15,7 +15,7 @@ ReactDOM.render(
       <Route path="/sign/:connexion" component={Sign} />
       <Route exact path="/user_profile/:id" component={UserProfile} /> {/* ADD :ID VARIABLE HERE TO GET RIGHT USER */}
       <Route exact path="/user_profile/:id/:edit" component={UserProfile} /> {/* ADD :ID VARIABLE HERE TO GET RIGHT USER */}
-      <Route path="/schools/:city" component={SchoolsList} />
+      <Route path="/schools/:city" component={SchoolCards} />
     </Switch>
   </BrowserRouter>,
 
