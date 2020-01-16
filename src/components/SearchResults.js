ef4639b4c6060828fb839108632ef64c78cc8faa
Mboulgour <mathieu.r.77@gmail.com>
2020-01-16 11:27:06 +0100

    search by cities working

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,7 +1,20 @@
-import React, { Component } from 'react'
-import Navbar from './Navbar'
+import React, { Component } from 'react';
+
+import CardItem from './CardItem';
+import Navbar from './Navbar';
+
+import fakeSchools from '../fakeData/fakeSchools';
 
 class SearchResults extends Component {
+  state = {
+    schools: undefined
+  }
+
+  componentDidMount = () =>{
+    let schoolsData = fakeSchools.filter(elm => elm.city === this.props.location.state.search.city) 
+    schoolsData.length > 0 && this.setState({ schools: schoolsData })
+  }
+
   render() {
     // GET SEARCH VALUES FROM COMPONENT SEARCHBAR
     const props = this.props.location.state 
@@ -10,9 +23,16 @@ class SearchResults extends Component {
         <Navbar />
         <div className="SearchResults">
           <h2 className="SearchResults__title">Résultats de votre recherche:</h2>
-          {props.search.speciality && <p>Speciality: {props.search.speciality}</p>}
-          {props.search.school && <p>School: {props.search.school}</p>}
-          {props.search.city && <p>City: {props.search.city}</p>}
+          <div className="SearchResults__results">
+            {props.search.city && 
+            this.state.schools ?
+              this.state.schools.map(school => 
+              <CardItem school={school} />
+            )
+            :
+            <h3>Aucune école trouvée pour votre recherche.</h3>
+          }
+          </div>
         </div>
       </>
     )
