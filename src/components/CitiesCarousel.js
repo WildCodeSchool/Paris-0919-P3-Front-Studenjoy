cc7c0119a7bed21b65d90d43a8fba34cf88478f5
Ibrahima Niass <cheroubawan@hotmail.fr>
2020-02-02 16:35:07 +0100

    tried to fix css carousel

--- src/components/CitiesCarousel.js
+++ src/components/CitiesCarousel.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import Slider from "react-slick";
-import CityItem from "./CityItem";
+import Slider from 'react-slick';
+import CityItem from './CityItem';
 import axios from 'axios';
 
 const sliderSettings = {
   dots: true,
   infinite: true,
   speed: 500,
-  slidesToShow: 4,
+  slidesToShow: 3,
   slidesToScroll: 1,
   initialSlide: 1,
   centerMode: true,
@@ -40,35 +40,43 @@ const sliderSettings = {
   ]
 };
 
-
 class CitiesCarousel extends React.Component {
   state = {
-    cities: undefined,
-  }
+    cities: undefined
+  };
 
-  componentDidMount = () =>{
-    axios.get("http://localhost:5000/schools")
-      .then(res => this.setState({
-        schools: res.data,
-        cities: res.data.map(city => city.city)
-      }))
-      .catch(err => console.log(err))
-    }
+  componentDidMount = () => {
+    axios
+      .get('http://localhost:5000/schools')
+      .then(res =>
+        this.setState({
+          schools: res.data,
+          cities: res.data.map(city => city.city)
+        })
+      )
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
       <section className="Cities" id="Cities">
         <Slider {...sliderSettings} className="Cities__container">
-            {this.state.schools && 
-            this.state.schools.map(school_city => 
+          {this.state.schools &&
+            this.state.schools.map(school_city => (
               <Link key={school_city.id} to={`/schools/${school_city.id}`}>
-                <CityItem city={school_city} numSchools={this.state.cities.filter(city => city === school_city.city).length}/>
+                <CityItem
+                  city={school_city}
+                  numSchools={
+                    this.state.cities.filter(city => city === school_city.city)
+                      .length
+                  }
+                />
               </Link>
-            )}
+            ))}
         </Slider>
       </section>
     );
   }
-};
+}
 
 export default CitiesCarousel;
